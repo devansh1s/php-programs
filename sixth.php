b44e471724daf8fec3771434297aaf2f771f481a
devansh1s <33063350+devansh1s@users.noreply.github.com>
2018-01-04 17:28:59 +0530

    Update sixth.php

--- sixth.php
+++ sixth.php
@@ -1,16 +1,12 @@
 <?php
-$s1 = "helloworld";
-$s2 = "HelloWorld";
-if (ctype_lower($s1)) {
-  echo("$s1 is in lower case");
-}
-else {
-  echo("$s1 is not in lower case");
-}
-if (ctype_lower($s2)) {
-  echo(" $s2 is in lower case");
-}
-else {
-  echo(" $s2 is not in lower case");
-}
+$s1 = "string";
+$s2 = "StrinG";
+if ($s1 == strtolower($s1))
+  echo("String s1 = $s1 is in lower case; ");
+else
+  echo("String s1 = $s1 is not in lower case; ");
+if ($s2 == strtolower($s2))
+  echo("String s2 = $s2 is in lower case; ");
+else
+  echo("String s2 = $s2 is not in lower case; ");
 ?>
